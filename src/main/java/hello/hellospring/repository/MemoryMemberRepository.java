0fe88eae350e6f16bdd5e5f3c01bdcbee91024a3
JUHO KIM <ksaid0203@gmail.com>
2022-09-16 19:34:59 +0900

    회원 관리 예제 - 백엔드 개발 : 회원 도메인과 리포지토리 만들기

---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,37 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static Long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    // Optional : Java 8에서 추가된 기능
+    // Null을 반환할 가능성이 있는 경우 Optional에 감싸 반환 할 수 있음
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
